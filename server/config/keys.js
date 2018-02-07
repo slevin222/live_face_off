0bd799cd1c7faad809a9f6958292b1b90861b2fb
Khaleel Younis <khaleelyounis1@gmail.com>
2018-02-07 12:03:14 -0800

    local/google auth setup

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://khaleel:final123@ds031845.mlab.com:31845/finalproject-dev',
+    googleClientID: '570303934194-q8l92a0frkqcred9jhmne4gs7t6jcaqj.apps.googleusercontent.com',
+    googleClientSecret: 'TCmxJ9DqDilO4J51bt54pPhM'
+};
