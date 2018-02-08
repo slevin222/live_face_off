0784f6e0dffe1edd3745fc98b3e2a5465f1a0bd4
Shawn Levin <shawnlevin22@gmail.com>
2018-02-07 22:08:37 -0800

    added images and game components

---
+++ client/src/components/gameBoard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../assets/css/game.css';
+import nineSpades from '../assets/images/9Spng';
+import sevenSpades from '../assets/images/7S.png';
+import jackClubs from '../assets/images/JC.png';
+import twoHearts from '../assets/images/2H.png';
+import threeClubs from '../assets/images/3C.png';
+
+export default props => {
+    return (
+        <div className="container">
+            <div className="playerTurn">Player 1</div>
+            <div className="playerHand">
+                <div className="playerCard0" style={{ backgroundImage: "url(" + nineSpades + ")" }}></div>
+                <div className="playerCard1" style={{ backgroundImage: "url(" + sevenSpades + ")" }}></div>
+                <div className="playerCard2" style={{ backgroundImage: "url(" + jackClubs + ")" }}></div>
+                <div className="playerCard3" style={{ backgroundImage: "url(" + twoHearts + ")" }}></div>
+                <div className="playerCard4" style={{ backgroundImage: "url(" + threeClubs + ")" }}></div>
+            </div>
+            <div className="bottomInfo">
+                <div className="bottom0">9 Points</div>
+                <div className="bottom1">10 Points</div>
+                <div className="bottom2">10 Points</div>
+                <div className="bottom3">2 Points</div>
+                <div className="bottom4">3 Points</div>
+            </div>
+            <div className="footer">
+                <button className="discardBtn" type="submit">Discard Cards</button>
+            </div>
+        </div>
+    );
+}
