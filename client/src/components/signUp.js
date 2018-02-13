0ab4bb0032420c87debb6021e307c71f39bb6c71
Khaleel Younis <khaleelyounis1@gmail.com>
2018-02-13 09:55:08 -0800

    added front end changes

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -1,52 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/signUpStyle.css';
+import axios from 'axios';
 
-export default (props) => {
-    return (
-        <div className="signUpBackground">
-            <div className="signUpArea">
-                <div className="row">
-                    <form className="col s12" action="https://localhost:5000/users/register" method="post">
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <label htmlFor="signUpUsername"></label>
-                                <input id="signUpUsername" type="text" name="name" className="validate" placeholder="Name" required />
+class SignUp extends Component {
+    constructor(props) {
+        super(props);
+    }
+    // postRequest() {
+    //     axios.post('http://localhost:5000/users/register')
+    //         .then(res => {
+    //             console.log('The response from the registration: ', resp);
+    //         });
+    // }
+    render() {
+        return (
+            <div className="signUpBackground">
+                <div className="signUpArea">
+                    <div className="row">
+                        <form className="col s12" action="http://localhost:5000/users/register" method="post">
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input id="signUpUsername" type="text" name="name" className="validate" placeholder="Full Name" required />
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <label htmlFor="signUpPassword"></label>
-                                <input id="signUpPassword" type="password" name="password" placeholder="Password" className="validate" required />
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input id="signUpEmail" type="email" name="email" placeholder="Email" className="validate" required />
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <label htmlFor="signUpPassword2"></label>
-                                <input id="signUpPassword" type="password" name="password2" placeholder="Confirm Password" className="validate" required />
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input id="signUpPassword" type="password" name="password" placeholder="Password" className="validate" required />
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <label htmlFor="signUpEmail"></label>
-                                <input id="signUpEmail" type="email" name="email" placeholder="Email" className="validate" required />
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input id="signUpPassword" type="password" name="password2" placeholder="Confirm Password" className="validate" required />
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="row">
-                            <div className='col s12 center-align'>
-                                <button type="submit" className='signUpBtn waves-effect waves-light btn'>Sign Up</button>
-                                <Link className='logInBtn waves-effect waves-light btn' to='/'>Cancel</Link>
+                            <div className="row">
+                                <div className='col s12 center-align'>
+                                    <button type="submit" className='signUpBtn waves-effect waves-light btn'>Sign Up</button>
+                                    <Link className='logInBtn waves-effect waves-light btn' to='/login'>Cancel</Link>
+                                </div>
                             </div>
-                        </div>
-
-                    </form>
-
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default SignUp
