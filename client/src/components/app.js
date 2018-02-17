0ce0b2d7d087763ea77dc2d3c31baa94ec889b73
Paul <paulmlee7@gmail.com>
2018-02-17 13:17:49 -0800

    created navbar with routing

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -10,6 +10,16 @@ import TokBox from './openTok';
 const App = () => {
     return (
         <div className="mainApp">
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to='/' className='brand-logo'>Live Face Off</Link>
+                    <ul className="right">
+                        <li><Link to='/lobby'>Lobby</Link></li>
+                        <li><Link to='/register'>Sign Up</Link></li>
+                        <li><Link to='/login'>Login</Link></li>
+                    </ul>
+                </div>
+            </nav>
             <Route path="/sessionpage" component={TokBox} />
             <Route path='/login' component={LoginPage} />
             <Route path='/register' component={signUp} />
