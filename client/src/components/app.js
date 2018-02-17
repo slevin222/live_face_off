01f63c35114504f2468a3ad30528ff8e9d5afdc4
Paul <paulmlee7@gmail.com>
2018-02-17 14:27:39 -0800

    completed navbar component. created a homepage and lobby. cleaned up app.js code

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,29 +1,24 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Navbar from './navbar';
+import Homepage from './homepage';
+import SignUp from './signUp';
 import LoginPage from './loginPage';
-import signUp from './signUp';
-import GameBoard from './gameBoard';
+import LobbyPage from './lobbyPage';
 import GamePage from './gamePage';
 import TokBox from './openTok';
 
 const App = () => {
     return (
         <div className="mainApp">
-            <nav>
-                <div className="nav-wrapper">
-                    <Link to='/' className='brand-logo'>Live Face Off</Link>
-                    <ul className="right">
-                        <li><Link to='/lobby'>Lobby</Link></li>
-                        <li><Link to='/register'>Sign Up</Link></li>
-                        <li><Link to='/login'>Login</Link></li>
-                    </ul>
-                </div>
-            </nav>
+            <Navbar/>
+            <Route exact path='/' component={Homepage} />
             <Route path="/sessionpage" component={TokBox} />
-            <Route path='/login' component={LoginPage} />
-            <Route path='/register' component={signUp} />
             <Route path='/gamepage' component={GamePage} />
+            <Route path='/lobby' component={LobbyPage} />
+            <Route path='/register' component={SignUp} />
+            <Route path='/login' component={LoginPage} />
         </div>
     )
 }
---
+++ client/src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <nav className='blue-grey darken-3'>
+            <div className="nav-wrapper ">
+                <Link style={{marginLeft: '10px'}} to='/' className='brand-logo'>Live Face Off</Link>
+                <ul className="right">
+                    <li><Link to='/sessionpage'>TEMP Sessionpage Link</Link></li>
+                    <li><Link to='/gamepage'>TEMP Gamepage Link</Link></li>
+                    <li><Link to='/lobby'>Lobby</Link></li>
+                    <li><Link to='/register'>Sign Up</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
