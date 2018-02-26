8d28e3241b7638cdd99ea6527db76fb4bb4df95a
Paul <paulmlee7@gmail.com>
2018-02-26 10:59:24 -0800

    added an about us component

---
+++ client/src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div className='container'>
+            <h1>About Us</h1>
+            <p>We made this</p>
+        </div>
+    )
+}
