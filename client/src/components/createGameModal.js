bcb91a43abc80d0e560742e352598d610b8dcbe0
Paul <paulmlee7@gmail.com>
2018-02-28 10:26:36 -0800

    completed create game modal

--- client/src/components/createGameModal.js
+++ client/src/components/createGameModal.js
@@ -5,7 +5,6 @@ import '../assets/css/createGameModal.css';
 class createGameModal extends Component {
 
     render(){
-
         let redirect = null;
         if(this.props.gameType === 'webcam'){
             redirect = '/camGame';
@@ -18,23 +17,22 @@ class createGameModal extends Component {
         };
 
         return (
-            <div className="createGameModal">
+            <div className="createGameModal" style={displayModal}>
                 <div className="createGameContent">
-
                     <div className='center-align'>
                         <h5>Deal 52</h5>
+                        <div className='divider'></div>
                         <h6>How to play</h6>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                             A alias esse, laborum non pariatur tempore!!! Lorem ipsum dolor sit amet,
                             consectetur adipisicing elit. Distinctio, officiis.</p>
                     </div>
-
+                    <div className='divider'></div>
                     <div className='center-align'>
                         <h5>Unique room key</h5>
-                        <div style={{margin: '12px 0'}}><span id='roomKeyStyle'>{this.props.roomKey}kkeykeykeykey</span></div>
-                        <h6>Share this with your friends and family!</h6>
+                        <div style={{margin: '16px 0'}}><span id='roomKeyStyle'>{this.props.roomKey}kkeykeykeykey</span></div>
+                        <h6>Share this key with your friends and family!</h6>
                     </div>
-
                     <div className='center-align' style={{marginTop: '18px'}}>
                         <Link to={redirect} className='btn blue-grey darken-2'>Join Room</Link>
                     </div>
