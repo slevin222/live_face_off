5bdfcde361ac6522aa3c221f8fa9f03dba1a2c00
Paul <paulmlee7@gmail.com>
2018-02-27 17:50:47 -0800

    completed winner modal on cam game page

--- client/src/components/camGame.js
+++ client/src/components/camGame.js
@@ -2,23 +2,53 @@ import React, { Component } from 'react';
 import TokBox from './openTok';
 import '../assets/css/camGame.css';
 import Chat from './chat';
-import LobbyPage from './lobbyPage';
-
+import CamGameWinModal from "./camGameWinModal";
 
 class CamGame extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
+        this.state = {
+            displayModal: false,
+            teamOneScore: 0,
+            teamTwoScore: 0,
+            winningTeam: ''
+        }
 
         this.displayWinner = this.displayWinner.bind(this);
+        this.closeWinModal = this.closeWinModal.bind(this);
+        this.handleScoreInput = this.handleScoreInput.bind(this);
+    }
+
+    handleScoreInput(event){
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        })
     }
 
     displayWinner() {
+        const { teamOneScore, teamTwoScore } = this.state;
+        let msg = (teamOneScore === teamTwoScore) ? 'Tie game!' : (teamOneScore > teamTwoScore) ? 'Team 1 wins!': 'Team 2 wins!';
 
+        this.setState({
+            displayModal: true,
+            winningTeam: msg
+        })
+    }
+
+    closeWinModal(){
+        this.setState({
+            displayModal: false,
+            winningTeam: ''
+        })
     }
 
     render() {
+        const { displayModal, teamOneScore, teamTwoScore, winningTeam } = this.state;
         const roomKeyId = sessionStorage.getItem("roomKey");
+
         return (
             <div className="webpage row s12">
                 <div className="col s3 score container">
@@ -27,24 +57,26 @@ class CamGame extends Component {
                         <div className="row">
                             <div className="col s5">
                                 <h6>Team 1</h6>
-                                <input className="score" type="number" placeholder="Enter Score" />
+                                <input id='teamOneScore' onChange={this.handleScoreInput} name='teamOneScore' value={teamOneScore} className="score" type="number" placeholder="Enter Scoreee" />
                             </div>
                             <div className="col s5">
                                 <h6>Team 2</h6>
-                                <input className="score" type="number" placeholder="Enter Score" />
+                                <input id='teamTwoScore' onChange={this.handleScoreInput} name='teamTwoScore' value={teamTwoScore} className="score" type="number" placeholder="Enter Score" />
                             </div>
                         </div>
                     </div>
                     <button onClick={this.displayWinner} className='btn blue-grey darken-2 center-align' id="winnerBtn">Display Winner</button>
                     <div>
-                        <Chat />
+                        {/*<Chat />*/}
                     </div>
                 </div>
                 <div className="col s9" >
-                    <TokBox />
+                    {/*<TokBox />*/}
                 </div>
+                <CamGameWinModal display={displayModal} close={this.closeWinModal} gameResult={winningTeam} teamOneScore={teamOneScore} teamTwoScore={teamTwoScore}/>
             </div>
         )
     }
 }
+
 export default CamGame;
---
+++ client/src/components/camGameWinModal.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../assets/css/camGameWinModal.css';
+
+class CamGameWinModal extends Component {
+
+    render(){
+        let displayModal = {
+            display: this.props.display ? 'block' : 'none'
+        };
+
+        return (
+            <div className='camGameWinModal' style={displayModal}>
+                <div className='camGameWinModalContent'>
+                    <h1>{this.props.gameResult}</h1>
+                    <h4>Final Score</h4>
+                    <h4>{this.props.teamOneScore} to {this.props.teamTwoScore}</h4>
+                    <button className='btn' onClick={this.props.close}>Close</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CamGameWinModal;
