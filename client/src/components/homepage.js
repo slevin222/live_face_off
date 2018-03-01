13417df29187bb24e475197948b77e640bf6db00
Crystal Shir Navarro <crystal.shir@gmail.com>
2018-03-01 15:24:26 -0800

    Pulling Shawn's changes

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import '../assets/css/homePage.css';
-// import LFOlanding from '../assets/images/LFOlanding.png';
 import LandingBG from '../assets/images/gameBG3.png'
 
 var styles = {
-    backgroundSize: 'contain',
+    backgroundSize: 'cover',
+    backroundRepeat: 'no-repeat',
     backgroundImage: 'url('+LandingBG+')'
 };
   
 class Homepage extends Component {
+    renderLinks(){
+        return [
+            <li key='0'>
+                <Link to='/register'>Sign Up</Link>
+            </li>,
+            <li key='1'>
+                <Link to='/login'>Login</Link>
+            </li>
+        ]
+    }
     render() {
         return (
          <div className='pageContainer' style={ styles }>
             <div className= 'row '>
-                 <div className='landing col s6'>
+                 <div className='landing col s5'>
                      <div className='mainDiv'>
                             <h1 className ="gameTitle home center center-align">Live Face Off </h1>
                             <h1 className ="gameTitle  home center center-align"></h1>
-                         
-                     </div>
+                    </div>
                 </div>
-            <div className = 'row'>
-                <div className='col s6 center center-align'>
-                         <h3 className='title'>What is Live Face Off?</h3>
-                         <p>Want to reconnect with a family member overseas? Need a place to hangout with buddies after a long day of work?  Live Face Off is the platform where you can easily spend some time with someone close within a few easy steps. </p>
-                     </div>
-                <div className='col s6 center center-align'>
-                         <h3 className='title'>Features</h3>
-                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non viverra turpis. Nam eu porttitor dolor. Nulla aliquet, sapien a posuere finibus, mauris felis lacinia augue, a auctor turpis nunc at urna. Donec elementum velit vitae neque viverra, id tincidunt enim rhoncus. Vivamus scelerisque ante metus. Proin ac sodales ligula. Aliquam bibendum vestibulum accumsan. Morbi malesuada dignissim est, et gravida augue ultrices nec. Fusce tincidunt vel turpis et tempus. Donec convallis augue quis imperdiet scelerisque. Quisque ut orci venenatis, lobortis velit sed, elementum mi. Praesent volutpat malesuada ultrices.</p>
-                     </div>
-                     <div className="divider brown"></div>
+                <div className='col s7 center center-align'>
+                    <div className='row'>
+                        <h3 className='title'>What is Live Face Off?</h3>
+                        <p>Want to reconnect with a family member overseas? Need a place to hangout with buddies after a long day of work?  Live Face Off is the platform where you can easily spend some time with someone close within a few easy steps. </p>
+                    </div>
+                    <div className='row'>
+                        <h3 className='title center center-align'>Features</h3>
+                        <div className='accountAccess'>
+                        <button id='signUpAcc' className='btn brown darken-4 waves-effect waves-light btn-large' style={{ marginTop: '23px' }}>Create a Free Account</button>
+                        </div>
+                    </div>
             </div>
-            <div className='row'>
-                <div className='titleLine col s10 offset-s1'>
-                     <h1 className='center center-align'> Features </h1>
-                </div>
-            </div>
-
-            
-            <div className='row homeRow'>
+        </div>
+        <div className='row homeRow'>
                 <div className ='featureLeft col s12 m4 l2  right-align'>
                     <div className='row center center-align'>
                         <h5>Login Friendly</h5>
@@ -60,12 +66,10 @@ class Homepage extends Component {
                     <p>We enhanced the experience with enabling you to communicate with your friends and family via video, voice, and text chat. If you have a slower connection or don't have a WebRTC compatible browser, you have the option to chat or turn off your voice and video.  </p>
                 </div>
             </div>
-            <div className='row'>
-           
-                </div>
-             </div>
         </div>
         )
     }
 }
+
+
 export default Homepage;
