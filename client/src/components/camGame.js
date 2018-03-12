bb81bb2be296c7ecd90404fbe6f2c957c63614b4
Shawn Levin <shawnlevin22@gmail.com>
2018-03-12 12:55:39 -0700

    fixed about picture to size on mobile

--- client/src/components/camGame.js
+++ client/src/components/camGame.js
@@ -86,10 +86,10 @@ class CamGame extends Component {
                                     </div>
                                 </div>
                                 <div className="row">
-                                    <div className="col s6 center-align">
+                                    <div className="col s6 center-align" id="infoBtn">
                                         <button onClick={this.displayInfo} className='btn teal accent-4 camGameBtn waves-effect waves-light'>Info</button>
                                     </div>
-                                    <div className="col s6 center-align">
+                                    <div className="col s6 center-align" id="endGameBtn">
                                         <button onClick={this.displayWinner} className="btn red accent-4 camGameBtn waves-effect waves-light">End Game</button>
                                     </div>
                                 </div>
