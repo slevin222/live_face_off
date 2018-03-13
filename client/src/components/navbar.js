eb095fb7849453eea8733dba4364b073797d002f
Khaleel Younis <khaleelyounis1@gmail.com>
2018-03-12 23:03:41 -0700

    fixed signup/login/navbar sizing, added css file for navbar, set min and max height so everything lines up perfectly

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,26 +3,27 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 import LFOlogoSM from '../assets/images/LFOlogoSM.png'
+import '../assets/css/navbar.css'
 
 class Navbar extends Component {
     renderLinks() {
         if (this.props.auth) {
             return [
-                <li key='0'>
-                    <Link to='/lobby'>Lobby</Link>
+                <li className="navbarLi" key='0'>
+                    <Link to='/lobby' className="navbarItem">Lobby</Link>
                 </li>,
-                <li key='1'>
-                    <a onClick={this.props.signOut}>Logout</a>
+                <li className="navbarLi" key='1'>
+                    <a onClick={this.props.signOut} className="navbarItem">Logout</a>
                 </li>
             ]
         }
 
         return [
-            <li key='0'>
-                <Link to='/register'>Sign Up</Link>
+            <li className="navbarLi" key='0'>
+                <Link to='/register' className="navbarItem">Sign Up</Link>
             </li>,
-            <li key='1'>
-                <Link to='/login'>Login</Link>
+            <li className="navbarLi" key='1'>
+                <Link to='/login' className="navbarItem">Login</Link>
             </li>
         ]
     }
@@ -31,9 +32,9 @@ class Navbar extends Component {
         return (
             <nav className='teal accent-4 navBar'>
                 <div className="nav-wrapper ">
-                    <Link style={{ marginLeft: '6%' }} to='/' className='brand-logo left'><img style={{ position: 'absolute', left: '-28%', top: '8%', width: '26%' }} src={LFOlogoSM} /><span>Live Face Off</span></Link>
-                    <ul className="right">
-                        <li><Link to='/about'>About</Link></li>
+                    <Link to='/' className='brand-logo left'><img className="navImg" src={LFOlogoSM} /><span>Live Face Off</span></Link>
+                    <ul className="right navbarUl">
+                        <li className="navbarLi"><Link to='/about' className="navbarItem">About</Link></li>
                         {this.renderLinks()}
                     </ul>
                 </div>
