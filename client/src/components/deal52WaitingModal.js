0ca26b73b2653e0dc0069e6668be5eded88591e7
Shawn Levin <shawnlevin22@gmail.com>
2018-03-16 15:02:18 -0700

    style

--- client/src/components/deal52WaitingModal.js
+++ client/src/components/deal52WaitingModal.js
@@ -9,15 +9,15 @@ class Deal52WaitingModal extends Component {
         };
 
         return (
-            <div className='waitingGameModal' style={displayModal}>
+            <div className='waitingGameModal' id="waitingModalContent" style={displayModal}>
                 <div className='waitingModalContent contentBorder'>
                     <div className='center-align'>
-                        <h4>Waiting For All Players</h4>
-                        <div>
+                        <h4 id="waitingText">Waiting For All Players</h4>
+                        {/* <div>
                             <h5>Unique room key</h5>
                             <div style={{ margin: '16px 0' }}><span className='roomKeyStyle'>{this.props.roomKey}</span></div>
                             <p>Share this key with your friends and family so they can join your room!</p>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
